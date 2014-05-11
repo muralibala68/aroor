83c5fc83307f7ac5d4bbdc97e345aea05c1cd262
Murali Bala <muralibala68@gmail.com>
2014-05-11 16:30:16 +0100

    tested in intellij / gradle

---
+++ scratch/src/test/java/org/bala/scratch/logbackTest.java
@@ -0,0 +1,26 @@
+package org.bala.scratch;
+
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.util.StatusPrinter;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by murali on 11/05/14.
+ */
+public class logbackTest {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(logbackTest.class);
+
+    public static void printLoggerState() {
+        LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+        StatusPrinter.print(lc);
+    }
+
+    @Test
+    public void myTest1() {
+        printLoggerState();
+        LOGGER.info("Hello, World!");
+    }
+}
