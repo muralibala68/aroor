3d9ede9f44a57cda94ba5d38ffb0659625f26090
Murali Bala <muralibala68@gmail.com>
2016-01-02 21:46:22 +0000

    further tidying of code

--- scratch/src/main/java/org/bala/functional/xface/AlgorithmStep.java
+++ scratch/src/main/java/org/bala/functional/xface/AlgorithmStep.java
@@ -2,18 +2,16 @@ package org.bala.functional.xface;
 
 import java.util.List;
 
+import org.bala.closure.SimpleAveragePrice;
 import org.bala.md.Trade;
 
 @FunctionalInterface
 public interface AlgorithmStep {
-	void execute();
-	
-default double calculateVWAP(final List<Trade> trades) {
-		double vwap = 0.0d;
-		//trades.forEach(t -> vwap = vwap + (t.getSize() * t.getPrice()) );
-		
-		
-		return vwap;
-	}
+    void execute();
+    
+    default double calculateAveragePrice(final List<Trade> trades) {
+        final SimpleAveragePrice simpleAveragePrice = new SimpleAveragePrice(trades);
+        return simpleAveragePrice.calculate().orElse(0.0d);
+    }
 
 }
--- scratch/src/main/java/org/bala/functional/xface/VWAPStep.java
+++ scratch/src/main/java/org/bala/functional/xface/VWAPStep.java
@@ -2,17 +2,15 @@ package org.bala.functional.xface;
 
 import java.util.List;
 
+import org.bala.closure.VolumeWeigtedAveragePrice;
 import org.bala.md.Trade;
 
 @FunctionalInterface
 public interface VWAPStep extends AlgorithmStep {
 
 	@Override
-	default double calculateVWAP(final List<Trade> trades) {
-		double vwap = 2.0d;
-		//trades.forEach(t -> vwap = vwap + (t.getSize() * t.getPrice()) );
-		
-		
-		return vwap;
+	default double calculateAveragePrice(final List<Trade> trades) {
+	    final VolumeWeigtedAveragePrice volumeWeigtedAveragePrice = new VolumeWeigtedAveragePrice(trades);
+	    return volumeWeigtedAveragePrice.calculate().orElse(0.0d);
 	}
 }
