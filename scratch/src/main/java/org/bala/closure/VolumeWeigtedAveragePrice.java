e14498adb2e2c7c5c674d9aac48d3e0cba8daed2
Murali Bala <muralibala68@gmail.com>
2016-01-02 21:23:49 +0000

    Further tidying

--- scratch/src/main/java/org/bala/closure/VolumeWeigtedAveragePrice.java
+++ scratch/src/main/java/org/bala/closure/VolumeWeigtedAveragePrice.java
@@ -9,117 +9,117 @@ import java.util.function.Predicate;
 import org.bala.md.Trade;
 
 public class VolumeWeigtedAveragePrice {
-	private final List<Trade> trades;
-	
-	public VolumeWeigtedAveragePrice(List<Trade> trades) {
-		this.trades = trades;
-	}
-	
-	public void printAllTrades_v1() {
-		trades.forEach( (Trade t) -> {
-			System.out.println(t); 
-			}
-		);
-	}
+    private final List<Trade> trades;
+    
+    public VolumeWeigtedAveragePrice(List<Trade> trades) {
+        this.trades = trades;
+    }
+    
+    public void printAllTrades_v1() {
+        trades.forEach( (Trade t) -> {
+            System.out.println(t); 
+            }
+        );
+    }
 
-	public void printAllTrades_v2() {
-		trades.forEach( t -> {
-			System.out.println(t); 
-			}
-		);
-	}
+    public void printAllTrades_v2() {
+        trades.forEach( t -> {
+            System.out.println(t); 
+            }
+        );
+    }
 
-	public void printAllTrades_v3() {
-		trades.forEach( t -> System.out.println(t) );
-	}
-	
-	public void printAllTrades() {
-		trades.forEach(System.out::println);
-	}
+    public void printAllTrades_v3() {
+        trades.forEach( t -> System.out.println(t) );
+    }
+    
+    public void printAllTrades() {
+        trades.forEach(System.out::println);
+    }
 
-	public long getAllTradesCount_v1() {
-		return trades.stream().mapToLong(t -> 1L).reduce(0, Long::sum);
-	}
+    public long getAllTradesCount_v1() {
+        return trades.stream().mapToLong(t -> 1L).reduce(0, Long::sum);
+    }
 
-	public long getAllTradesCount_v2() {
-		return trades.stream().mapToLong(t -> 1L).sum();
-	}
+    public long getAllTradesCount_v2() {
+        return trades.stream().mapToLong(t -> 1L).sum();
+    }
 
-	public long getAllTradesCount() {
-		return trades.stream().count();
-	}
+    public long getAllTradesCount() {
+        return trades.stream().count();
+    }
 
-	public void printValidTrades() {
-		trades.stream().filter(Trade::isValid).forEach(System.out::println);
-	}
+    public void printValidTrades() {
+        trades.stream().filter(Trade::isValid).forEach(System.out::println);
+    }
 
-	public long getValidTradesCount() {
-		return trades.stream().filter(Trade::isValid).count();
-	}
+    public long getValidTradesCount() {
+        return trades.stream().filter(Trade::isValid).count();
+    }
 
-	public void printAllTradeValues_v1() {
-		trades.forEach( t -> System.out.println( t.getValue() ) );
-	}
+    public void printAllTradeValues_v1() {
+        trades.forEach( t -> System.out.println( t.getValue() ) );
+    }
 
-	public void printAllTradeValues() {
-		trades.stream().map(Trade::getValue).forEach(System.out::println);
-	}
-	
-	public void printValidTradeValues_v1() {
-		final Predicate<Trade> validTrade = (Trade t) -> t.getPrice() > 0.0d && t.getSize() > 0.0d;
-		trades.stream().filter(validTrade).map(Trade::getValue).forEach(System.out::println);
-	}
-	
-	public void printValidTradeValues_v2() {
-		final Predicate<Trade> validTrade = Trade::isValid;
-		trades.stream().filter(validTrade).map(Trade::getValue).forEach(System.out::println);
-	}
-	
-	public void printValidTradeValues_v3() {
-		trades.stream().filter(Trade::isValid).map(Trade::getValue).forEach(System.out::println);
-	}
+    public void printAllTradeValues() {
+        trades.stream().map(Trade::getValue).forEach(System.out::println);
+    }
+    
+    public void printValidTradeValues_v1() {
+        final Predicate<Trade> validTrade = (Trade t) -> t.getPrice() > 0.0d && t.getSize() > 0.0d;
+        trades.stream().filter(validTrade).map(Trade::getValue).forEach(System.out::println);
+    }
+    
+    public void printValidTradeValues_v2() {
+        final Predicate<Trade> validTrade = Trade::isValid;
+        trades.stream().filter(validTrade).map(Trade::getValue).forEach(System.out::println);
+    }
+    
+    public void printValidTradeValues_v3() {
+        trades.stream().filter(Trade::isValid).map(Trade::getValue).forEach(System.out::println);
+    }
 
-	private Double getSumOfAllTradeSizes_v1() {
-		final Double identity = new Double(0.0d);
-		final BiFunction<Double, Trade, Double> tradeSizeAccumulator = (Double totalSize,Trade trade) -> totalSize + trade.getSize();
-		final BinaryOperator<Double> tradeSizeCombiner = (Double ts1, Double ts2) -> ts1 + ts2;
-		final Double sumOfAllTradeSizes = trades.parallelStream().reduce(identity, tradeSizeAccumulator, tradeSizeCombiner);
-		return sumOfAllTradeSizes;
-	}
+    private Double getSumOfAllTradeSizes_v1() {
+        final Double identity = new Double(0.0d);
+        final BiFunction<Double, Trade, Double> tradeSizeAccumulator = (Double totalSize,Trade trade) -> totalSize + trade.getSize();
+        final BinaryOperator<Double> tradeSizeCombiner = (Double ts1, Double ts2) -> ts1 + ts2;
+        final Double sumOfAllTradeSizes = trades.parallelStream().reduce(identity, tradeSizeAccumulator, tradeSizeCombiner);
+        return sumOfAllTradeSizes;
+    }
 
-	private Double getSumOfAllTradeSizes_v2() {
-		return trades.parallelStream().reduce(0.0d, (ts,t)-> ts + t.getSize(), (ts1,ts2)-> ts1 + ts2);
-	}
+    private Double getSumOfAllTradeSizes_v2() {
+        return trades.parallelStream().reduce(0.0d, (ts,t)-> ts + t.getSize(), (ts1,ts2)-> ts1 + ts2);
+    }
 
-	private Double getSumOfAllTradeSizes_v3() {
-		return trades.stream().mapToDouble(Trade::getSize).sum();
-	}
+    private Double getSumOfAllTradeSizes_v3() {
+        return trades.stream().mapToDouble(Trade::getSize).sum();
+    }
 
-	private Optional<Double> getSumOfAllTradeSizes() {
-		final Optional<Double> sumOfAllTradeSizes = trades.stream().map(t -> t.getSize()).reduce((ts1,ts2)-> ts1 + ts2);
-		return sumOfAllTradeSizes;
-	}
+    private Optional<Double> getSumOfAllTradeSizes() {
+        final Optional<Double> sumOfAllTradeSizes = trades.stream().map(t -> t.getSize()).reduce((ts1,ts2)-> ts1 + ts2);
+        return sumOfAllTradeSizes;
+    }
 
-	private Double getSumOfAllTradeValues_v1() {
-		return trades.stream().mapToDouble(Trade::getValue).sum();
-	}
+    private Double getSumOfAllTradeValues_v1() {
+        return trades.stream().mapToDouble(Trade::getValue).sum();
+    }
 
-	private Optional<Double> getSumOfAllTradeValues() {
-		final Optional<Double> sumOfAllTradeValues = trades.stream().map(Trade::getValue).reduce((tv1,tv2) -> tv1 + tv2);
-		return sumOfAllTradeValues;
-	}
+    private Optional<Double> getSumOfAllTradeValues() {
+        final Optional<Double> sumOfAllTradeValues = trades.stream().map(Trade::getValue).reduce((tv1,tv2) -> tv1 + tv2);
+        return sumOfAllTradeValues;
+    }
 
-	public Optional<Double> calculate() {
-		final Optional<Double> sumOfAllTradeValues = getSumOfAllTradeValues();
-		sumOfAllTradeValues.ifPresent(System.out::println);
+    public Optional<Double> calculate() {
+        final Optional<Double> sumOfAllTradeValues = getSumOfAllTradeValues();
+        sumOfAllTradeValues.ifPresent(System.out::println);
 
-		final Optional<Double> sumOfAllTradeSizes = getSumOfAllTradeSizes();
-		sumOfAllTradeSizes.ifPresent(System.out::println);
+        final Optional<Double> sumOfAllTradeSizes = getSumOfAllTradeSizes();
+        sumOfAllTradeSizes.ifPresent(System.out::println);
 
-		final Optional<Double> vwap = sumOfAllTradeValues.isPresent() && sumOfAllTradeSizes.isPresent() ?
-			  Optional.of(sumOfAllTradeValues.get() / sumOfAllTradeSizes.get()) :
-			  Optional.empty();
-		vwap.ifPresent(System.out::println);
-		return vwap;
-	}
+        final Optional<Double> vwap = sumOfAllTradeValues.isPresent() && sumOfAllTradeSizes.isPresent() ?
+              Optional.of(sumOfAllTradeValues.get() / sumOfAllTradeSizes.get()) :
+              Optional.empty();
+        vwap.ifPresent(System.out::println);
+        return vwap;
+    }
 }
--- scratch/src/test/java/org/bala/closure/VolumeWeigtedAveragePriceTest.java
+++ scratch/src/test/java/org/bala/closure/VolumeWeigtedAveragePriceTest.java
@@ -7,53 +7,52 @@ import static org.hamcrest.Matchers.lessThan;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.OptionalDouble;
 
 import org.bala.md.Trade;
 import org.junit.Test;
 
 public class VolumeWeigtedAveragePriceTest {
-	
-	public static void main(String[] args) {
-		final List<Trade> trades = setupTrades();
-		final VolumeWeigtedAveragePrice vwap = new VolumeWeigtedAveragePrice(trades);
-		vwap.printAllTrades();
-		vwap.printAllTradeValues();
-		Optional<Double> vwaprice = vwap.calculate();
-		vwaprice.ifPresent(System.out::println);
-	}
 
-	private static List<Trade> setupTrades() {
-		final List<Trade> trades = new ArrayList<>();
-		trades.add(new Trade("I1",   0,   0,   System.currentTimeMillis() - 6000));
-		trades.add(new Trade("I1",  11, 100.5, System.currentTimeMillis() - 5000));
-		trades.add(new Trade("I1", 200, 100.1, System.currentTimeMillis() - 4000));
-		trades.add(new Trade("I1",  31, 100.4, System.currentTimeMillis() - 3000));
-		trades.add(new Trade("I1",  41, 100.3, System.currentTimeMillis() - 2000));
-		trades.add(new Trade("I1", 100, 100.2, System.currentTimeMillis() - 1000));
-		return trades;
-	}	
-	
-	@Test
-	public void testAllTradesCount() {
-		final List<Trade> trades = setupTrades();
-		final VolumeWeigtedAveragePrice vwap = new VolumeWeigtedAveragePrice(trades);
-		assertThat(vwap.getAllTradesCount(),is(6L));
-		assertThat(vwap.getAllTradesCount_v1(),is(6L));
-		assertThat(vwap.getAllTradesCount_v2(),is(6L));
-	}
+    public static void main(String[] args) {
+        final List<Trade> trades = setupTrades();
+        final VolumeWeigtedAveragePrice vwap = new VolumeWeigtedAveragePrice(trades);
+        vwap.printAllTrades();
+        vwap.printAllTradeValues();
+        Optional<Double> vwaprice = vwap.calculate();
+        vwaprice.ifPresent(System.out::println);
+    }
 
-	@Test
-	public void testValidTradesCount() {
-		final List<Trade> trades = setupTrades();
-		final VolumeWeigtedAveragePrice vwap = new VolumeWeigtedAveragePrice(trades);
-		assertThat(vwap.getValidTradesCount(),is(5L));
-	}
+    private static List<Trade> setupTrades() {
+        final List<Trade> trades = new ArrayList<>();
+        trades.add(new Trade("I1",   0,   0,   System.currentTimeMillis() - 6000));
+        trades.add(new Trade("I1",  11, 100.5, System.currentTimeMillis() - 5000));
+        trades.add(new Trade("I1", 200, 100.1, System.currentTimeMillis() - 4000));
+        trades.add(new Trade("I1",  31, 100.4, System.currentTimeMillis() - 3000));
+        trades.add(new Trade("I1",  41, 100.3, System.currentTimeMillis() - 2000));
+        trades.add(new Trade("I1", 100, 100.2, System.currentTimeMillis() - 1000));
+        return trades;
+    }
 
-	@Test
-	public void testVWAPPrice() {
-		final List<Trade> trades = setupTrades();
-		final VolumeWeigtedAveragePrice vwap = new VolumeWeigtedAveragePrice(trades);
-		assertThat(Math.abs(vwap.calculate().get() - 100.18328), is(lessThan(0.0001)));
-	}
+    @Test
+    public void testAllTradesCount() {
+        final List<Trade> trades = setupTrades();
+        final VolumeWeigtedAveragePrice vwap = new VolumeWeigtedAveragePrice(trades);
+        assertThat(vwap.getAllTradesCount(),is(6L));
+        assertThat(vwap.getAllTradesCount_v1(),is(6L));
+        assertThat(vwap.getAllTradesCount_v2(),is(6L));
+    }
+
+    @Test
+    public void testValidTradesCount() {
+        final List<Trade> trades = setupTrades();
+        final VolumeWeigtedAveragePrice vwap = new VolumeWeigtedAveragePrice(trades);
+        assertThat(vwap.getValidTradesCount(),is(5L));
+    }
+
+    @Test
+    public void testVWAPPrice() {
+        final List<Trade> trades = setupTrades();
+        final VolumeWeigtedAveragePrice vwap = new VolumeWeigtedAveragePrice(trades);
+        assertThat(Math.abs(vwap.calculate().get() - 100.18328), is(lessThan(0.0001)));
+    }
 }
